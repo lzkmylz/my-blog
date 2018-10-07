f37ae3ce2c85b4ef78c0413a3524926f7aa4fa66
lzkmylz <lzkmylz@163.com>
2018-10-07 23:25:30 +0800

    format css class name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,39 +2,39 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <div className="headerBox">
-    <div className="headerLeftContext">
-      <div className="headerSearchLogoBox">
+  <div className="header-box">
+    <div className="header-left-context">
+      <div className="header-search-logo">
         <img
-          className="headerSearchLogoImg"
+          className="header-logo-img"
           src="/images/headerSearchLogo.jpg"
           alt="Header logo"
         />
       </div>
-      <div className="headerSearchBox">
+      <div className="header-search-box">
         <input
           type="text"
-          className="headerSearchInput"
+          className="header-search-input"
           placeholder="Search"
         />
       </div>
     </div>
-    <div className="headerCenterContext">
-      <div className="centerBox">
-        <div className="topCenterBox">
+    <div className="header-center-context">
+      <div className="two-col-box">
+        <div className="top-box">
           <p>lzkmylz的个人博客</p>
         </div>
-        <div className="topBottomBox">
+        <div className="bottom-box">
           <p>Blog of lzkmylz</p>
         </div>
       </div>
     </div>
-    <div className="headerRightContext">
+    <div className="header-right-context">
       <ul>
-        <li><Link className="headerLink" to="/games">博文</Link></li>
-        <li><Link className="headerLink" to="/articles">小游戏</Link></li>
-        <li><Link className="headerLink" to="/photos">照片</Link></li>
-        <li><Link className="headerLink" to="/git">Github</Link></li>
+        <li><Link className="header-link" to="/games">博文</Link></li>
+        <li><Link className="header-link" to="/articles">小游戏</Link></li>
+        <li><Link className="header-link" to="/photos">照片</Link></li>
+        <li><Link className="header-link" to="/git">Github</Link></li>
       </ul>
     </div>
   </div>
