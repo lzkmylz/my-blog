a553eb4c99ce0095a6fc520d17165c3abf0569d7
lzkmylz <lzkmylz@163.com>
2018-10-08 23:48:32 +0800

    add personal photo and describe

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
 const HomePage = () => (
-  <div>
-    <p>This is HomePage.</p>
+  <div className="homepage-box">
+    <div className="homepage-context-box">
+      <div className="homepage-photo-box">
+        <img src="/images/lzkmylz.jpg" alt="mine" />
+      </div>
+      <div className="homepage-text-box">
+        <p className="homepage-text-name">lzkmylz</p>
+        <p>程序猿/快乐肥宅/单身狗</p>
+      </div>
+    </div>
   </div>
 );
 
